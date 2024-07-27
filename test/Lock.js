a6e75e2809b3023744262308d9f1502b396b2d8a
Aashray Gupta <127435012+AashrayGithub@users.noreply.github.com>
2024-07-27 15:30:57 +0530

    Update Lock.js

--- test/Lock.js
+++ test/Lock.js
@@ -1,2 +1,2 @@
-// I have written the testing code in foundry. Because this is a hardhat project, I will write the testing code in Readme file.
+// I have written the testing code in foundry. Because this is a hardhat project, I will upload a separate notepad file.
 //Ignore the name Lock.js
